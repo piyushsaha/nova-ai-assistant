90bde909f9fd6929065ca59f682f49160089dc0f
Piyush Saha <itsmepiyushsaha@gmail.com>
2023-09-29 17:26:48 +0530

    add: dummy messages + message component

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,10 +1,40 @@
+import { useState } from 'react';
 import { Dimensions, Image, StyleSheet, View } from 'react-native';
 
 import Features from '../components/Features';
+import Messages from '../components/Messages';
 
 const { height } = Dimensions.get('window');
+const dummyMessages = [
+  {
+    role: 'user',
+    content: 'How are you?',
+  },
+  {
+    role: 'assistant',
+    content: "I'm fine, How may i help you today.",
+  },
+  {
+    role: 'user',
+    content: 'How are you?',
+  },
+  {
+    role: 'assistant',
+    content: "I'm fine, How may i help you today.",
+  },
+  {
+    role: 'user',
+    content: 'create an image of a dog playing with cat',
+  },
+  {
+    role: 'assistant',
+    content:
+      'https://storage.googleapis.com/pai-images/ae74b3002bfe4b538493ca7aedb6a300.jpeg',
+  },
+];
 
 const HomeScreen = () => {
+  const [messages, setMessages] = useState(dummyMessages);
   return (
     <View style={styles.homeScreenContainer}>
       <View style={{ alignItems: 'center' }}>
@@ -13,9 +43,7 @@ const HomeScreen = () => {
           style={styles.botLogo}
         />
       </View>
-      <View>
-        <Features />
-      </View>
+      {messages.length === 0 ? <Features /> : <Messages messages={messages} />}
     </View>
   );
 };
