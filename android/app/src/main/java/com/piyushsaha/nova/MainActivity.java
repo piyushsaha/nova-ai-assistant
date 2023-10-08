1af236d82f0363377ae15bf22f3aac4a7e646c9d
Piyush Saha <itsmepiyushsaha@gmail.com>
2023-10-08 13:35:34 +0530

    update: android prebuild

--- android/app/src/main/java/com/piyushsaha/nova/MainActivity.java
+++ android/app/src/main/java/com/piyushsaha/nova/MainActivity.java
@@ -1,4 +1,4 @@
-package com.piyushsaha.nebula;
+package com.piyushsaha.nova;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/piyushsaha/nova/MainApplication.java
+++ android/app/src/main/java/com/piyushsaha/nova/MainApplication.java
@@ -1,4 +1,4 @@
-package com.piyushsaha.nebula;
+package com.piyushsaha.nova;
 
 import android.app.Application;
 import android.content.res.Configuration;
